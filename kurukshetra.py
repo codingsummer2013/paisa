1c124cd759d1e3ab76bb4596b38c45908c7c61f4
deepakagrawal <codingsummer2013@gmail.com>
2022-09-28 11:13:52 +0530

    add

--- kurukshetra.py
+++ kurukshetra.py
@@ -149,7 +149,7 @@ def market_closed():
 
 while True:
     if (not market_closed()) or skipped_market_check:
-        # becho_re()
+        becho_re()
         khareedo_re()
         # becho_re()
         time.sleep(60)
